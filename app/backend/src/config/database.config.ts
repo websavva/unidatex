90f4c4817b3ba03eab561b5cbc4adbe2785f7e1f
WebSavva <websavvap@gmail.com>
2024-11-04 01:08:00 +0300

    fix: HOST variable was added for database config

--- app/backend/src/config/database.config.ts
+++ app/backend/src/config/database.config.ts
@@ -1,6 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export const loadDatabaseConfig = registerAs('database', () => ({
+  HOST: process.env.POSTGRES_HOST!,
   PORT: +process.env.POSTGRES_PORT!,
   USER: process.env.POSTGRES_USER!,
   PASSWORD: process.env.POSTGRES_PASSWORD!,
