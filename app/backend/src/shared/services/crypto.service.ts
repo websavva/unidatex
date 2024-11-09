6dbd15a807d1a68547e32bf5c40bdd7adf31019e
WebSavva <websavvap@gmail.com>
2024-11-09 19:07:44 +0300

    feat: password crypto utils were added to CryptoService

--- app/backend/src/shared/services/crypto.service.ts
+++ app/backend/src/shared/services/crypto.service.ts
@@ -1,10 +1,19 @@
 import { randomBytes } from 'node:crypto';
 
 import { Injectable } from '@nestjs/common';
+import bcrypt from 'bcryptjs';
 
 @Injectable()
 export class CryptoService {
   generateRandomHash(bytes = 6) {
     return randomBytes(bytes).toString('hex');
+  }
+
+  hashifyPassword(password: string) {
+    return bcrypt.hash(password, 8);
+  }
+
+  comparePassword(plainPassword: string, passwordHash: string) {
+    return bcrypt.compare(plainPassword, passwordHash);
   }
 }
