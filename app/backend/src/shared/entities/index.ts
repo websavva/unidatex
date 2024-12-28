b0cd0fc7beba016ed71c35ceb1c11eb9d77eb5e1
WebSavva <websavvap@gmail.com>
2024-12-28 12:20:21 +0300

    Missing entities were added

--- app/backend/src/shared/entities/index.ts
+++ app/backend/src/shared/entities/index.ts
@@ -3,12 +3,16 @@ import { UserPhotoEntity } from './user-photo.entity';
 import { UserProfileViewEntity } from './user-profile-view.entity';
 import { UserFavoriteEntity } from './user-favorite.entity';
 import { CityEntity } from './city.entity';
+import { ChatEntity } from './chat/chat.entity';
+import { MessageEntity } from './chat/message.entity';
 
 export * from './user.entity';
 export * from './user-photo.entity';
 export * from './user-profile-view.entity';
 export * from './user-favorite.entity';
 export * from './user.entity';
+export * from './chat/chat.entity';
+export * from './chat/message.entity';
 
 export const allEntities = [
   UserEntity,
@@ -16,4 +20,6 @@ export const allEntities = [
   UserProfileViewEntity,
   UserFavoriteEntity,
   CityEntity,
+  ChatEntity,
+  MessageEntity,
 ];
